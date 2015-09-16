5ea953acc7431e9fec69d27bdafc900ec27e0ad6
Claire Nessler <clrnessler@gmail.com>
2015-09-16 12:40:33 -0600

    removed page flash

--- public/javascripts/dom.js
+++ public/javascripts/dom.js
@@ -1,11 +1,13 @@
 
 $(document).ready(function (){
-  $("#top").click(function (){
+  $("#top").click(function (e){
+          e.preventDefault()
           $('html, body').animate({
               scrollTop: $("#skills").offset().top
           }, 2000);
   });
-  $("#contactTop").click(function (){
+  $("#contactTop").click(function (e){
+          e.preventDefault()
           $('html, body').animate({
               scrollTop: $("#contact").offset().top
           }, 2000);
